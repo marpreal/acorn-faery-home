eb92b1d75955b0c678f8697391e560b3bca17eba
Marta Pretel <marta.ruidosordo@gmail.com>
2024-08-28 09:29:31 +0200

    add calendar

--- src/components/Homepage/components/Books/Books.js
+++ src/components/Homepage/components/Books/Books.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import bgImage from '../../../../assets/images/bg.jpg'; 
 import frameImage from '../../../../assets/images/container07.png'; 
-import { dataByMonth } from './data';
+import { dataByYear } from './data'; // Import the data
 import './Books.css';
 
 const Books = () => {
+  const years = Object.keys(dataByYear);
+
+  // Get the current year
+  const currentYearString = new Date().getFullYear().toString();
+
+  // Find the index of the current year
+  const initialYearIndex = years.indexOf(currentYearString);
+
+  // Set the initial state based on the current year
+  const [currentYearIndex, setCurrentYearIndex] = useState(
+    initialYearIndex !== -1 ? initialYearIndex : 0
+  );
+
+  // Get the current year and books for that year
+  const currentYear = years[currentYearIndex];
+  const booksForCurrentYear = dataByYear[currentYear];
+
+  const handleNextYear = () => {
+    setCurrentYearIndex((prevIndex) => {
+      const nextIndex = prevIndex + 1;
+      return nextIndex < years.length ? nextIndex : prevIndex;
+    });
+  };
+
+  const handlePreviousYear = () => {
+    setCurrentYearIndex((prevIndex) => {
+      const prevIndexAdjusted = prevIndex - 1;
+      return prevIndexAdjusted >= 0 ? prevIndexAdjusted : prevIndex;
+    });
+  };
+
   return (
     <div
       className="bg-cover bg-center min-h-screen flex justify-center items-center relative"
@@ -25,26 +56,44 @@ const Books = () => {
       </div>
 
       <div className="relative flex flex-col items-center justify-center w-4/5 max-w-lg z-10 mt-24">
-      <div
-  className="relative w-full h-[35vh] p-4 border-4 border-yellow-800 rounded-lg bg-opacity-30 bg-white overflow-auto scroll-container"
->
-  <h1 className="text-2xl font-bold mb-6 text-center">Books of the Year</h1>
-  {Object.keys(dataByMonth).map((month) => (
-    <div key={month} className="mb-4">
-      <h2 className="text-lg font-semibold mb-2">{month}</h2>
-      
-      {dataByMonth[month].length > 0 && (
-        <>
-          <ul className="list-disc pl-5">
-            {dataByMonth[month].map((book, index) => (
-              <li key={index} className="mb-1">{book}</li>
-            ))}
-          </ul>
-        </>
-      )}
-    </div>
-  ))}
-</div>
+        <div
+          className="relative w-full h-[35vh] p-4 border-4 border-yellow-800 rounded-lg bg-opacity-30 bg-white overflow-auto scroll-container"
+        >
+          <div className="flex justify-between items-center mb-6">
+            <button
+              onClick={handlePreviousYear}
+              className="px-4 py-2 bg-yellow-800 text-white rounded-full hover:bg-yellow-700 transition-transform transform hover:scale-105"
+            >
+              &lt; Prev
+            </button>
+            <h1 className="text-2xl font-bold text-center">{currentYear}</h1>
+            <button
+              onClick={handleNextYear}
+              className="px-4 py-2 bg-yellow-800 text-white rounded-full hover:bg-yellow-700 transition-transform transform hover:scale-105"
+            >
+              Next &gt;
+            </button>
+          </div>
+
+          {Object.keys(booksForCurrentYear).length > 0 ? (
+            Object.keys(booksForCurrentYear).map((month) => (
+              <div key={month} className="mb-4">
+                <h2 className="text-lg font-semibold mb-2">{month}</h2>
+                <ul className="list-disc pl-5">
+                  {booksForCurrentYear[month].length > 0 ? (
+                    booksForCurrentYear[month].map((book, index) => (
+                      <li key={index} className="mb-1">{book}</li>
+                    ))
+                  ) : (
+                    <p>No books available for {month}.</p>
+                  )}
+                </ul>
+              </div>
+            ))
+          ) : (
+            <p className="text-center">No books available for this year.</p>
+          )}
+        </div>
       </div>
 
       <Link 
--- src/components/Homepage/components/Books/data.js
+++ src/components/Homepage/components/Books/data.js
@@ -1,37 +1,50 @@
-export const dataByMonth = {
-  January: [
-    'Flox de los colores',
-    'Adiós, Fairy Oak',
-    'El destino de un hada',
-    'La historia perdida'
-  ],
-  February: [],
-  March: [
-    'Lores y Damas',
-    'El estrecho sendero entre deseos'
-  ],
-  April: [
-    'To hell and back again. My Burzum Story.',
-    'Dungeon Synth - The Rebirth of the legend.',
-    'Siega',
-    'Por si las voces vuelven',
-    'Gothan, GasLight'
-  ],
-  May: [
-    'How to Sell a Haunted House',
-    "The Southern Book Club's Guide to Slaying Vampires",
-    'Meditaciones de Marco Aurelio',
-    'Wilt',
-    'Beyond Order'
-  ],
-  June: [
-    'Drácula'
-  ],
-  July: [
-    'The Fellowship of the Ring'
-  ],
-  August: [
-    'The Two Towers',
-    'The Return of the King'
-  ]
+export const dataByYear = {
+  "2023": {
+    January: ["Los crímenes del Monograma", "Macbeth", "1984", "Romeo y Julieta", "Dune I"],
+    February: ["Dune II, Dune III"],
+    March: ["Brujas de viaje", "El oso y el ruiseñor", "La chica en la torre", "Mouse Guard", "El invierno de la bruja"],
+    June: ["De qué hablo cuando hablo de correr", "La guía del autoestopista galáctico", "Narnia Chronicles: The Magician’s Nephew", "Narnia Chronicles: The Lion, the Witch and the Wardrobe"],
+    July: ["El exorcismo de mi mejor amiga"],
+    August: ["Jugada maestra"],
+    September: ["La sombra del viento", "Wicked", "Hijo de bruja", "La liga de los hombres extraordinarios I"],
+    October: ["La liga de los hombres extraordinarios II"],
+    November: ["Los chicos del valle", "Fairy Oak: El Secreto de las Gemelas", "Fairy Oak: El encanto de la oscuridad", "Fairy Oak: El poder de la luz"],
+    December: ["Fairy Oak: El capitán Grissam y el amor", "Fairy Oak: Los hechiceros días de Shirley"]
+  },
+  "2024": {
+    January: [
+      'Flox de los colores',
+      'Adiós, Fairy Oak',
+      'El destino de un hada',
+      'La historia perdida'
+    ],
+    March: [
+      'Lores y Damas',
+      'El estrecho sendero entre deseos'
+    ],
+    April: [
+      'To hell and back again. My Burzum Story.',
+      'Dungeon Synth - The Rebirth of the legend.',
+      'Siega',
+      'Por si las voces vuelven',
+      'Gothan, GasLight'
+    ],
+    May: [
+      'How to Sell a Haunted House',
+      "The Southern Book Club's Guide to Slaying Vampires",
+      'Meditaciones de Marco Aurelio',
+      'Wilt',
+      'Beyond Order'
+    ],
+    June: [
+      'Drácula'
+    ],
+    July: [
+      'The Fellowship of the Ring'
+    ],
+    August: [
+      'The Two Towers',
+      'The Return of the King'
+    ]
+  },
 };
