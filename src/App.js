ef5bbb204e82bec62d19065336f90816aee9dc74
Marta Pretel <marta.ruidosordo@gmail.com>
2024-08-30 08:59:50 +0200

    added basename

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/Homepage/HomePage';
 import Books from './components/Homepage/components/Books/Books';
 
 const App = () => {
   return (
-    <Router basename="/acorn-faery-home">
+    <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/books" element={<Books />} />
